e91b477cee443cbe65d06f37b9b9cdf1686719ca
uxkp <uxkpmail@gmail.com>
2025-09-11 14:15:27 +0530

    try removing turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ["fra.cloud.appwrite.io"],
   },
+  experimental: {
+    turbo: false,
+    webpackBuildWorker: true,
+  },
+  webpack: (config) => {
+    return config;
+  },
 };
 
 export default nextConfig;
