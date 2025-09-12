9712cb1a2390ef4c9cf45be4280d82e80ed08070
uxkp <uxkpmail@gmail.com>
2025-09-12 21:46:00 +0530

    slight width change

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -23,8 +23,8 @@ export default async function PostPage({ params }) {
       <Navbar page="home" />
       <div className="relative w-full min-h-screen pb-10 bg-[#19191C]">
         <div className="absolute -z-1 blur-[100px] top-0 left-0 w-[50vw] h-[100vh] bg-[#1f192d] rounded-br-full" />
-        <div className="pt-32 mx-[8vw] flex justify-center">
-          <div className="bg-[#27272A] rounded-2xl shadow-lg w-full max-w-4xl text-white overflow-hidden">
+        <div className="pt-32 mx-[4vw] flex justify-center">
+          <div className="bg-[#27272A] rounded-2xl shadow-lg w-full text-white overflow-hidden">
             <div className="p-10">
               <span
                 className="inline-block px-3 py-1 text-xs font-medium text-black rounded-full mb-3"
