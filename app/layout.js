3a699269f0115d1e4b3fbc6563a332d471f22b71
uxkp <uxkpmail@gmail.com>
2025-09-14 11:39:58 +0530

    fixing black text in light-mode devices

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} antialiased !important`}>
+      <body
+        className={`${poppins.className} antialiased text-white !important`}
+      >
         <HolyLoader color="#d6c7ff" height="0.2rem" />
         <Providers>
           <AlertProvider>{children}</AlertProvider>
